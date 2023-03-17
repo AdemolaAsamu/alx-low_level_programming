74a82a0cadc72b929dee5f8fe9bd607ecd218fb3
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-03-17 12:16:25 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: The program prints out the
+ * alphabets in lower case, doesnt allow printf or puts
+ * strictly uses putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* The for Statement loop to print A - Z */
+	int i;
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 25 ; i >= 0 ; i--)
+	{
+		putchar(alphabet[i]);
+
+	}
+	putchar('\n');
+	return (0);
+}
