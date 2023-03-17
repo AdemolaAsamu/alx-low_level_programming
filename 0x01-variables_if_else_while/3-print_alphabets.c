457b1342d36d288ceb3c41ae8cacf4beddd54d41
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-03-17 10:09:39 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: The program prints out the
+ * alphabets in Upper and lower case, doesnt allow printf or puts
+ * strictly uses putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* The for Statement loop to print A - Z */
+	int i;
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	char upperalph[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (i = 0 ; i < 26 ; i++)
+	{
+		putchar(alphabet[i]);
+
+	}
+
+	for (i = 0 ; i < 26 ; i++)
+	{
+		putchar(upperalph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
