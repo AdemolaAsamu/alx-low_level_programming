f796669de940f0ac15c5be8de0d80e6f11e1dcf8
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-03-17 03:38:18 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - File of printing random numbers
+ * return: 0 success
+ */
+int main(void)
+{       
+        int n;
+        
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+/* If statements to confirm the type of entry selection*/
+if (n > 0)
+{	
+printf(" %d is positive \n",n);
+return (n);
+}
+else if (n == 0)
+{
+	printf("%d is zero \n",n);
+	return (n);
+}
+else if (n < 0 )
+{
+	printf("%d is negative \n",n);
+	return (n);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+This is the  readme file for the if else state ment excercise 
+
+step 0 : positive anything is better
