3415f3220462c467b6926fef0e07a18b4adca2e7
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-03-17 04:11:52 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,26 +6,26 @@
  * return: 0 success
  */
 int main(void)
-{       
-        int n;
-        
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 /* If statements to confirm the type of entry selection*/
-if (n > 0)
-{	
-printf(" %d is positive \n",n);
-return (n);
-}
-else if (n == 0)
-{
-	printf("%d is zero \n",n);
-	return (n);
-}
-else if (n < 0 )
-{
-	printf("%d is negative \n",n);
-	return (n);
-}
-return (0);
+	if (n > 0)
+	{	
+		printf("%d is positive \n",n);
+		return (n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero \n",n);
+		return (n);
+	}
+	else if (n < 0 )
+	{
+		printf("%d is negative \n",n);
+		return (n);
+	}
+	return (0);
 }
