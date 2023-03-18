94bc22718e4b2c72ef355445253a42a172ca0ddb
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-03-18 11:03:44 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ *
+ * Description: This is a program that prints
+ * Combination of numbers below 100
+ * in accending order, no repeading number
+ * 03 == 30 therefore doesnt work
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* The declaration of variables */
+	int i, j;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		for (j = 48 ; j < 58 ; j++)
+		{
+			if (i > j)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+				putchar(' ');
+				if (i == j)
+				{
+					continue;
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
