f4fc879e14caf8895c6a03d4c5f14a0c8198aa4b
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-03-21 15:08:52 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer - prints out the 24 hours
+ * times
+ *
+ * @i, j, k, l: all defined variables
+ *
+ * Return: null
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j < 52; j++)
+		{
+			for (k = 48; k < 54; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
