70f47aa420403b8f8a48fc6215ba3db745ca6a15
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-03-21 16:44:36 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,15 @@
 /**
  * add - function that adds two ints
  *
+ * @a: variable 1
+ * @b: the second variable
+ *
  * Return: add
  */
 int add(int a, int b)
 {
 	int c;
-	
+
 	c = a + b;
 
 	return (c);
