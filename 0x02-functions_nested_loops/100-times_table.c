bc0965ede11fe7b266c0700a8310e52c1c17bef8
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-03-22 11:52:59 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 	int j;
 	int p;
 
-	if (n >= 0 || n <= 0)
+	if (n >= 0 || n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -31,11 +31,13 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar(p + '0');
 				}
 				else if (p >= 10)
 				{
 					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((p / 10) + '0');
 					_putchar((p % 10) + '0');
