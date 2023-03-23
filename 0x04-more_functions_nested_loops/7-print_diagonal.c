c8273d1b5e1a4e8b72f4dc4b4bad28fd217a176e
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-03-23 12:44:08 -0700

    Doing the hard things

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,8 +5,29 @@
  *
  * @n: This is the number of times diagonal is printed
  *
- * Return: 0 (success)
  */
 void print_diagonal(int n)
 {
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
 
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
