fcc102557a190fbde769433c4fb7c19ad490cd11
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-04-03 13:13:35 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - This function replaces the first n bytes
+ * of the memory area pointed to by s with the constant
+ * byte b
+ * @s: A pointer to the memory area to be filled
+ * @b: the constant byte to fill the memory area with
+ * @n: The number of bytes to fill with the constant byte
+ *
+ * Return: (s)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,8 @@
+0x07-pointers_arrays_strings
+
+This is the directory formed for the pointer of pointer tasks 
+
+This is also known as double pointers 
+
+step 1 - create a main file 
+step 2 create 0-memset.c
