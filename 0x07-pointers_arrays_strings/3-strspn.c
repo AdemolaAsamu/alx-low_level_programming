4915d60eb559c9ccd807b24e8ad39a62b35b9596
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-04-04 07:05:31 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,7 +2,7 @@
 
 /**
  * _strspn - function that gets the length
- * @s: input search string 
+ * @s: input search string
  * @accept: string containing character list
  * Return: Always 0
  */
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 		check = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if(s[i] == accept[j])
+			if (s[i] == accept[j])
 			{
 				k++;
 				check = 1;
