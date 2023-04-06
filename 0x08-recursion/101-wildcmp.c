957369731936b5d8f0d56e19bc02fa6afa5c0347
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-04-06 00:26:36 -0700

    Doing hard Things

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * wildcmp - compares similarity of strings
+ *
+ * @s1: first input string
+ * @s2: second input string
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1))
+		{
+			return (1);
+		}
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+		{
+			return (1);
+		}
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
