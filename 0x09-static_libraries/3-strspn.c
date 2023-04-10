96da2b36ff9ac7a0be9cfb272b057f98848d290b
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-04-09 23:08:18 -0700

    Creating static Library

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length
+ * @s: input search string
+ * @accept: string containing character list
+ * Return: Always 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k, check;
+
+	k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+				check = 1;
+			}
+		}
+		if (check == 0)
+		{
+			return (k);
+		}
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring
+ * @haystack: input
+ * @needle: input
+ *
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *w = needle;
+		char *v = haystack;
+
+		while (*v == *w && *w != '\0')
+		{
+			v++;
+			w++;
+		}
+		if (*w == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
