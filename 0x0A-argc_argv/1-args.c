8865d38e194cf05adab2e0c30f232adc47ec2a42
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-04-10 00:30:19 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry to print the arguments of a program
+ *
+ * @argc: This is the number of arguments
+ * @argv: This is the pointer to arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
