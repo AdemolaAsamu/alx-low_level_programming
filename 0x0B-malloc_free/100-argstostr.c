d3f83409858d22639d8ea4e7db859df567288469
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-04-11 03:21:31 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,27 +16,19 @@ char *argstostr(int ac, char **av)
 
 	l = 0;
 	j = 0;
-
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
-		{
 			l++;
-		}
 	}
 	l += ac;
-
 	result = malloc(sizeof(char) * l + 1);
-
 	if (result == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
