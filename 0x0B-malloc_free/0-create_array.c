5160041eb16fbb51999fee164aa9c97a2711178f
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-04-11 00:06:47 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - Function creates a dynamic array
+ *
+ * @size: this is the input size of array
+ * @c: the character input
+ *
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *A;
+	unsigned int i;
+
+	A = malloc(size * sizeof(char));
+	if (size == 0 || A == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		A[i] = c;
+	}
+	return (0);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,6 @@
+This is a readme file for the Malloc Tasks which Involved the Following
+1. Float like a butterly, sting like a bee. 
+Write a function that crates an array of chars and initializes it with a specific char
+Prototype: char *create_array(unsigned int size, char c);
+Returns NULL if size = 0
+Returns a pointer to the array, or null if it fails
