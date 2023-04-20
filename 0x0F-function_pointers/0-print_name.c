a1eece9ede97c54297c0bfae45e39400a2cd8c71
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-04-20 02:35:27 -0700

    0X0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - main function entry point
  * @name: Input
@@ -8,5 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
