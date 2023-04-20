f52846fb6c6a1669af9b32e8335a98fe6ee04c64
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-04-20 14:18:49 -0700

    variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,12 +8,10 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list all;
-	unsigned int i;
-	unsigned int sum;
+	unsigned int i, sum = 0;
 
 	va_start(all, n);
 
-	sum = 0;
 	if (n == 0)
 		return (0);
 
