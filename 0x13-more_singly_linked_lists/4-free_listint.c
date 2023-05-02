f13f4dcf70127d9bd9339f033c1fa65d2f613a95
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-05-02 12:32:02 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint - free the list
+ * @head: pointer to the head of list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *cnode;
+	for (; head != NULL; head = head->next)
+	{
+		cnode = head;
+		free(cnode);
+	}
+}
