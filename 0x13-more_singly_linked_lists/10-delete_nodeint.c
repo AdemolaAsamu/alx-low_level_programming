db6aeb738b53a012a458d8bca4749144c14f0321
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-05-02 15:00:10 -0700

    Task to 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *curr, *prev;
 	unsigned int i;
 
-	occur = *head;
+	curr = *head;
 	prev = NULL;
 	i = 0;
 	if (*head == NULL)
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		prev = curr;
 		curr = curr->next;
-		i++
+		i++;
 	}
 	if (i != index || curr == NULL)
 		return (-1);
