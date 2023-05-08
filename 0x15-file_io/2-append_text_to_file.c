12a7cd42098f2d8264ed905718535664f2630f96
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-05-08 14:55:53 -0700

    2-append text

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: name of the file to append
+ * @text_content: content to add
+ * Return: 1 success, -1 fail
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+	int len;
+
+	len = strlen(text_content);
+	if (filename == NULL)
+		return (-1);
+
+	fp = fopen(filename, "a");
+	if (fp == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fclose(fp);
+		return (1);
+	}
+
+	if ((int)fwrite(text_content, sizeof(char), len, fp) != (int)len)
+	{
+		fclose(fp);
+		return (-1);
+	}
+	fclose(fp);
+	return (1);
+}
