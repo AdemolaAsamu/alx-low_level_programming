38c314ee6a0e25ee5fa38739075a1a362fa9e160
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-05-10 00:11:05 -0700

    Replenish

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	fp = fopen(filename, "a");
-	if (fp == NULL)
+	if (fp < 0)
 		return (-1);
 	if (text_content == NULL)
 	{
