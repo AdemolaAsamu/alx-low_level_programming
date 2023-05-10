dc39e4871c1a6e07fe52294e590501dd96e64d5c
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-05-10 00:28:24 -0700

    2-append_text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,17 +10,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	FILE *fp;
 	int len;
 
-	len = strlen(text_content);
 	if (filename == NULL)
 		return (-1);
 
 	fp = fopen(filename, "a");
 	if (fp == NULL)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		fclose(fp);
-		return (-1);
+		len = strlen(text_content);
 	}
 
 	if ((int)fwrite(text_content, sizeof(char), len, fp) != (int)len)
