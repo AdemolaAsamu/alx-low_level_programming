0406931acce601b79c382162a22bcdaf104815c3
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-05-10 03:55:21 -0700

    Copy File Contant

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,7 +13,8 @@ void print_holder(char *holder, size_t letters);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+#define BUFFER_SIZE 1024
+int main(int argc, char *file[]);
 
 
 
