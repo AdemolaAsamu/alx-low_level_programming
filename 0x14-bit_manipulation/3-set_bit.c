05f6977f01a8286f0fbb5d8da81b91254402cf69
AdemolaAsamu <Ademola.Asamu@gmail.com>
2023-07-07 00:01:35 +0200

    Set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit - fxn that sets the value of a bit to 1 at an index
+ * @n: input pointer to the unsigned long integer
+ * @index: the indec to be modified
+ * Return: 1 for success and -1 for fail
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int checkr;
+
+	checkr = sizeof(unsigned long int) * 8;
+	if (!n || index >= checkr)
+		return (-1);
+	*n |= (1UL << index);
+	return (1);
+}
