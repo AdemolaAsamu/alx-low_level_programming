4d04cf3be98cfd127c082a3a358e0d06451dde61
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-07-10 14:02:13 -0700

    Test Using egalement

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(holder);
 		return (0);
 	}
-	for (i = 0; i < len_read; i++)
+	for (i = 0; i <= len_read; i++)
 		_putchar(holder[i]);
 	len_write = fwrite(holder, sizeof(char), len_read, stdout);
 	if (len_write != len_read)
